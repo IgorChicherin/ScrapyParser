660d4e1a6059214c6814ad5438d06e8dbdaa8248
ashzak990@gmail.com <ashzak990@gmail.com>
2017-08-04 16:13:06 +0300

    wisell big sizes output

--- ScrapyParser/spiders/wisell_spider.py
+++ ScrapyParser/spiders/wisell_spider.py
@@ -1,6 +1,7 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import Selector
+from scrapy.http import Request
 
 from ScrapyParser.items import WisellItemLoader, SpidersItem
 
@@ -12,10 +13,30 @@ class WisellSpider(CrawlSpider):
     allowed_domains = ['wisell.ru']
 
     rules = [Rule(LinkExtractor(restrict_xpaths=['//*[@id="catalog-lements-id"]'],
-                                allow='https://wisell.ru/catalog/platya/([A-Za-z0-9-]+)'),
+                                allow='https://wisell.ru/catalog/\w+/([A-Za-z0-9-]+)'),
                   callback='parse_item'),
+             # Rule(LinkExtractor(restrict_xpaths=['//*[@id="size-interval-tabs"]/li[1]/a'],
+             #                    allow='https://wisell.ru/catalog/platya/([A-Za-z0-9-]+)'),
+             #      callback='parse_item'),
              Rule(LinkExtractor(restrict_xpaths=['//*[@id="main-catalog"]/footer[1]/div/ul/li[6]']), follow=True)]
 
+    def check_small_sizes(self, response):
+        selector = Selector(response)
+        # loader = WisellItemLoader(SpidersItem(), selector)
+        small_link = selector.xpath('//*[@id="size-interval-tabs"]/li[1]/a/@href').extract()[0]
+        # loader.add_value('url', response.url)
+        # loader.add_value('name', small_link)
+        # self.log(small_link)
+        # return loader.load_item()
+        urls = list()
+        urls.append(response.url)
+        if small_link != '#size_rang-2' or small_link != '#size_rang-cont-1':
+            urls.append('http://wisell.ru%s' %
+                        (selector.xpath('//*[@id="size-interval-tabs"]/li[1]/@data-url').extract()[0]))
+        # self.log(urls)
+        for url in urls:
+            self.log('yep')
+            yield Request(url=url, callback=self.parse_item, dont_filter=True)
 
     def parse_item(self, response):
         selector = Selector(response)
