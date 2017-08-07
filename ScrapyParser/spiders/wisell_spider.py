c7c9df9a6509e8082565e29c531ceb139415e7bf
ashzak990@gmail.com <ashzak990@gmail.com>
2017-08-07 16:48:33 +0300

    переход в карточки малых размеров

--- ScrapyParser/spiders/wisell_spider.py
+++ ScrapyParser/spiders/wisell_spider.py
@@ -4,7 +4,7 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import Selector
 from scrapy.http import Request, Response
-import requests
+
 
 from ScrapyParser.items import WisellItemLoader, SpidersItem
 
@@ -23,21 +23,41 @@ class WisellSpider(CrawlSpider):
              #      callback='parse_item'),
              Rule(LinkExtractor(restrict_xpaths=['//*[@id="main-catalog"]/footer[1]/div/ul/li[6]']), follow=True)]
 
+    # def start_requests(self):
+    #     for url in self.start_urls:
+    #         yield Request(url, callback=self.parse_item)
+
     def parse_item(self, response):
+        Item = SpidersItem()
         selector = Selector(response)
         loader = WisellItemLoader(SpidersItem(), selector)
-        loader.add_value('url', response.url)
-        loader.add_xpath('name', '//h1/text()')
-        loader.add_xpath('price', '//*[@id="currency_tab-1"]/div/div[2]/span/span/text()')
-        sizes_list = selector.xpath('//*[@id="size_rang-1"]/div/ul/li/label//span/text()').extract()
-        sizes_list.remove(sizes_list[0])
-        loader.add_value('sizes', sizes_list)
-        loader.add_value('site', 'wisell')
-        small_size_link = 'http://wisell.ru%s' % (selector.xpath('//*[@id="size-interval-tabs"]'
-                                                                 '/li[1]/@data-url').extract()[0])
-
-        response = Response(url=small_size_link)
-        self.log(response.body)
+        Item['name'] = selector.xpath('//h1/text()').extract()[0]
+        # loader.add_value('url', response.url)
+        # loader.add_xpath('name', '//h1/text()')
+        # loader.add_xpath('price', '//*[@id="currency_tab-1"]/div/div[2]/span/span/text()')
+        # sizes_list = selector.xpath('//*[@id="size_rang-1"]/div/ul/li/label//span/text()').extract()
+        # sizes_list.remove(sizes_list[0])
+        # loader.add_value('sizes', sizes_list)
+        # loader.add_value('site', 'wisell')
+        small_url = selector.xpath('//*[@id="size-interval-tabs"]/li/@data-url').extract()
+        # self.log(small_url)
+        if small_url[0]:
+            small_size_link = 'http://wisell.ru%s' % (small_url[0])
+            request = Request(small_size_link, callback=self.parse_small_size)
+            request.meta['item'] = Item
+            yield request
+        elif len(small_url) == 1:
+            yield Item
+        # response = Response(url=small_size_link)
+        # self.log(response.body)
         # loader.add_value('url2', response.url)
         # loader.add_xpath('name2', '//h1/text()')
-        return loader.load_item()
+        # return loader.load_item()
+
+
+    def parse_small_size(self, response):
+        Item = response.meta['item']
+        selector = Selector(response)
+        Item['name2'] = selector.xpath('//h1/text()').extract()[0]
+        # self.log(Item)
+        yield Item
