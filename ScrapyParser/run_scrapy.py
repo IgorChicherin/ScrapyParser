b489e87eda764c48ea468af6ef3f9ac0f7e61ffc
ashzak990@gmail.com <ashzak990@gmail.com>
2017-08-11 11:31:46 +0300

    Created WooCommerce connection function

--- ScrapyParser/run_scrapy.py
+++ ScrapyParser/run_scrapy.py
@@ -1,3 +1,5 @@
+import os
+
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
@@ -10,8 +12,21 @@ from ScrapyParser.spiders.wisell_spider import WisellSpider
 from ScrapyParser.spiders.primalinea_spider import PrimalineaSpider
 from ScrapyParser.spiders.avigal_spider import AvigalSpider
 
+# WooCommerce REST API
+from woocommerce import API
 
-def main():
+# Sync WooCommerce Module
+from ScrapyParser.woo_sync_db import compare_dress, del_item
+
+# Krasa CSV Parser
+from ScrapyParser.krasa_parser import krasa_parse
+
+
+def spiders_reactor():
+    '''
+    Run spiders in Twisted reactor
+    :return: boolean
+    '''
     configure_logging()
     runner = CrawlerRunner(get_project_settings())
     runner.crawl(BigmodaSpider)
@@ -22,11 +37,33 @@ def main():
     d = runner.join()
     d.addBoth(lambda _: reactor.stop())
     reactor.run()
+    return True
 
+
+def create_woo_conn():
+    '''
+    Create connection with WooCommerce REST API
+    :return: WooCommerce API connection
+    '''
+    files = ['добавить удалить размеры.txt', 'добавить удалить карточки.txt', 'errors.txt']
+    for file in files:
+        if os.path.exists(file):
+            os.remove(file)
+
+    with open('keys.txt', 'r') as file:
+        keys = [line.strip() for line in file]
+
+    consumer_key = keys[0]
+    consumer_secret = keys[1]
+
+    wcapi = API(
+        url='http://big-moda.com',
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        wp_api=True,
+        version="wc/v2",
+    )
+    return wcapi
 
 if __name__ == '__main__':
-    main()
-
-
-
-
+    spiders_reactor()
